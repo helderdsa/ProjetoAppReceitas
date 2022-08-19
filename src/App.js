7757cd32c5d277a7e28ea4843cd66310306a946c
helderdsa <helder_dsa@hotmail.com>
2022-08-19 19:35:00 -0300

    requisitos 2 a 4

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
-      <Route path="/foods" component={ Recipes } />
+      {/* <Route path="/foods" component={ Recipes } />
       <Route path="/drinks" component={ Recipes } />
       <Route path="/foods/:id-da-receita" component={ RecipeDetails } />
       <Route path="/drinks/:id-da-receita" component={ RecipeDetails } />
@@ -15,7 +16,7 @@ function App() {
       <Route path="/drinks/{id-da-receita}/in-progress" component={ RecipeInProgress } />
       <Route path="/done-recipes" component={ DoneRecipe } />
       <Route path="/favorite-recipes" component={ FavoriteRecipes } />
-      <Route path="/profile" component={ Profile } />
+      <Route path="/profile" component={ Profile } /> */}
     </Switch>
   );
 }
---
+++ src/pages/Login.jsx
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+
+function Login() {
+  const [emailData, setEmailData] = useState('');
+  const [passwordData, setPasswordData] = useState('');
+  const [enabledBtn, setEnableBtn] = useState(true);
+  const history = useHistory();
+
+  const validateUser = () => {
+    const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+$/i;
+    const validateEmail = emailRegex.test(emailData);
+    const minDigit = 6;
+    const validatePassword = passwordData.length > minDigit;
+    if (validateEmail && validatePassword) {
+      setEnableBtn(false);
+    } else {
+      setEnableBtn(true);
+    }
+  };
+
+  const submitUser = () => {
+    localStorage.setItem('user', { email: emailData });
+    localStorage.setItem('mealsToken', 1);
+    localStorage.setItem('cocktailsToken', 1);
+    history.push('/foods');
+  };
+
+  useEffect(() => {
+    validateUser();
+  }, [emailData, passwordData]);
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <label htmlFor="emailInput">
+        Email
+        <input
+          type="email"
+          id="emailInput"
+          data-testid="email-input"
+          value={ emailData }
+          onChange={ ({ target }) => setEmailData(target.value) }
+        />
+      </label>
+      <label htmlFor="passwordInput">
+        Password
+        <input
+          type="password"
+          id="passwordInput"
+          data-testid="password-input"
+          value={ passwordData }
+          onChange={ ({ target }) => setPasswordData(target.value) }
+        />
+      </label>
+      <button
+        type="button"
+        disabled={ enabledBtn }
+        data-testid="login-submit-btn"
+        onClick={ submitUser }
+      >
+        Enter
+      </button>
+    </div>
+  );
+}
+
+export default Login;
