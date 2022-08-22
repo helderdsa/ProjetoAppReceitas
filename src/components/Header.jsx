e2a8ba98fbd298cbe10adf2ba46fcc8e746d43e3
Erik Yamamoto <erik.ymmt@gmail.com>
2022-08-22 18:23:03 -0300

    requiremet 9 done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
@@ -6,6 +6,8 @@ import searchIcon from '../images/searchIcon.svg';
 function Header() {
   const history = useHistory();
   const { location: { pathname } } = history;
+
+  const [searchBar, setSearchBar] = useState(false);
 
   const verifyPathname = () => {
     switch (pathname) {
@@ -46,16 +48,26 @@ function Header() {
       {
         (pathname === '/drinks' || pathname === '/foods')
           ? (
-            <img
-              src={ searchIcon }
-              alt="profile icon"
-              data-testid="search-top-btn"
-            />
+            <button
+              type="button"
+              onClick={ () => setSearchBar(!searchBar) }
+            >
+              <img
+                src={ searchIcon }
+                alt="search icon"
+                data-testid="search-top-btn"
+              />
+            </button>
+
           )
           : (
             <div />
           )
       }
+      { searchBar
+        ? <input type="text" placeholder="search recipe" data-testid="search-input" />
+        : null}
+
     </header>
   );
 }
