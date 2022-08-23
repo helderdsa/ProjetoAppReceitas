9f055bbce086e463f282e3ed167df4b1c1454bbe
Erik Yamamoto <erik.ymmt@gmail.com>
2022-08-23 14:35:46 -0300

    add const in recipeInProgress test

--- src/tests/RecipeInProgress.test.js
+++ src/tests/RecipeInProgress.test.js
@@ -2,6 +2,15 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import RecipeInProgress from '../pages/RecipeInProgress';
 
+// const recipePhotoTestId = 'recipe-photo';
+// const recipeTitleTestId = 'recipe-title';
+// const shareBtnTestId = 'share-btn';
+// const favoriteBtnTestId = 'favorite-btn';
+// const recipeCategoryTestId = 'recipe-category';
+// const instructionsTestId = 'instructions';
+// const finishBtnTestId = 'finish-recipe-btn';
+// to do test id steps
+
 describe('Login page test', () => {
   it('render elements on page', () => {
     render(<RecipeInProgress />);
