12f7644dffc2a286edeba6c3402afc678cf13e21
Erik Yamamoto <erik.ymmt@gmail.com>
2022-08-24 18:08:35 -0300

    requirement 21 - done

--- src/components/FilterButtons.jsx
+++ src/components/FilterButtons.jsx
@@ -1,16 +1,41 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchDrinks, fetchMeals } from '../redux/actions';
 
-function FilterButtons({ categories }) {
+function FilterButtons({ categories, isFood }) {
+  const dispatch = useDispatch();
   const maxLenght = 5;
   const formatedCategories = categories
     .map((categorie) => categorie.strCategory)
     .slice(0, maxLenght);
+
+  const requestCategory = (category) => {
+    if (isFood) {
+      const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`;
+      dispatch(fetchMeals(url));
+    } else {
+      const url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${category}`;
+      dispatch(fetchDrinks(url));
+    }
+  };
+
+  const resetCategory = () => {
+    if (isFood) {
+      const url = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+      dispatch(fetchMeals(url));
+    } else {
+      const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+      dispatch(fetchDrinks(url));
+    }
+  };
+
   return (
     <div>
       <button
         type="button"
-        data-testid="all-category-filter"
+        data-testid="All-category-filter"
+        onClick={ resetCategory }
       >
         All
       </button>
@@ -20,6 +45,7 @@ function FilterButtons({ categories }) {
             type="button"
             key={ index }
             data-testid={ `${cat}-category-filter` }
+            onClick={ () => requestCategory(cat) }
           >
             {cat}
           </button>
